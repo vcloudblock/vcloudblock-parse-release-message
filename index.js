5a89d9b47b5634fb9979b0168e27666b0c2c7af4
ArthurZheng <869705086@qq.com>
2022-02-25 18:40:46 +0800

    Handling abnormal input

--- index.js
+++ index.js
@@ -3,33 +3,36 @@ const { NodeHtmlMarkdown } = require('node-html-markdown');
 const parseMessage = (message, option = {}) => {
     const messageObj = {};
 
-    if (option.html) {
-        message = NodeHtmlMarkdown.translate(message)
-            .replace(/\* \*\*/g, '- **')
-            .replace(/\*\*  \n/g, '**\n')
-            .replace(/\n   /g, '\n    ')
-        console.log("message:", message);
-    }
+    if (typeof message === 'string') {
 
-    const mutiLanguageMessage = message.split('##').filter((item) => item !== '');
-    mutiLanguageMessage.forEach(element => {
-        const structuredInformation = element.split('- ');
-        const tile = structuredInformation[0].replace(/\r\n/g, '');
-        const infos = structuredInformation.slice(1);
+        if (option.html) {
+            message = NodeHtmlMarkdown.translate(message)
+                .replace(/\* \*\*/g, '- **')
+                .replace(/\*\*  \n/g, '**\n')
+                .replace(/\n   /g, '\n    ')
+        }
 
-        const language = tile.slice(tile.indexOf('(') + 1, tile.indexOf(')'));
+        const mutiLanguageMessage = message.split('##').filter((item) => item !== '');
+        mutiLanguageMessage.forEach(element => {
+            const structuredInformation = element.split('- ');
+            const tile = structuredInformation[0].replace(/\r\n/g, '');
+            const infos = structuredInformation.slice(1);
 
-        messageObj[`${language}`] = {};
+            const language = tile.slice(tile.indexOf('(') + 1, tile.indexOf(')'));
 
-        infos.forEach(info => {
-            const details = info.split('    ');
-            const subTitle = details[0].replace(/(?:\r\n|\*)/g, '');
-            const descriptions = details.slice(1).map(item => item.replace(/\r\n/g, ''));
-            messageObj[`${language}`][`${subTitle}`] = descriptions;
+            messageObj[`${language}`] = {};
+
+            infos.forEach(info => {
+                const details = info.split('    ');
+                const subTitle = details[0].replace(/(?:\r\n|\*)/g, '');
+                const descriptions = details.slice(1).map(item => item.replace(/\r\n/g, ''));
+                messageObj[`${language}`][`${subTitle}`] = descriptions;
+            });
         });
-    });
 
-    return messageObj;
+        return messageObj;
+    }
+    return null;
 }
 
 module.exports = parseMessage;
